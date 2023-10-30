ef14c56a76f577912b54a0c7fb8ced582060e9ed
muskan <muskanbharti.cse2020@dscet.ac.in>
2023-10-30 15:29:34 +0530

    pt commit

---
+++ src/Print_Pattern.java
@@ -0,0 +1,18 @@
+public class Print_Pattern {
+    public static void main(String[] args) {
+        for (int i =10; i >=1 ; i--) {
+            for (int j = 0; j <i ; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+
+        }
+    }
+}
---
+++ src/practice_only.java
@@ -0,0 +1,21 @@
+public class practice_only {
+    public static void main(String[] args) {
+        for (int i = 11; i >=1; i--) {
+            for (int j = 0; j <i ; j++) {
+
+
+                System.out.print("*"+" ");
+            }
+            System.out.print("\n");
+        }
+
+        for (int i = 1; i <=11 ; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*"+" ");
+
+            }
+            System.out.print("\n");
+
+        }
+    }
+}
